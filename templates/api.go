0d7049bdb73610f24078c6dcaf831a5b49951d93
Tom Burns <tom.burns@jadedpixel.com>
2014-12-08 03:20:39 +0000

    add api binding

---
+++ templates/api.go
@@ -0,0 +1,49 @@
+package <%= @package %>
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type API struct {
+	URI    string
+	Token  string
+	Secret string
+	client *http.Client
+}
+
+type errorResponse struct {
+	Errors map[string]interface{} `json:"errors"`
+}
+
+func (api *API) request(endpoint string, method string, params map[string]interface{}, body io.Reader) (result *bytes.Buffer, status int, err error) {
+	if api.client == nil {
+		api.client = &http.Client{}
+	}
+
+	uri := fmt.Sprintf("%s/%s", api.URI, endpoint)
+	req, err := http.NewRequest(method, uri, body)
+	if err != nil {
+		return
+	}
+
+	req.SetBasicAuth(api.Token, api.Secret)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := api.client.Do(req)
+	fmt.Printf("resp %v err %v", resp, err)
+	if err != nil {
+		return
+	}
+
+	status = resp.StatusCode
+
+	result = &bytes.Buffer{}
+	if _, err = io.Copy(result, resp.Body); err != nil {
+		return
+	}
+
+	return
+}
